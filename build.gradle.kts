a59a28ce271ef0bd261ba73c3153462130a1ec4d
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-26 16:01:23 -0400

    Add manifest download task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     id("java")
     id("application")
+    id("de.undercouch.download") version "5.7.0"
 }
 
 group = "klaxon.klaxon.imperator"
 version = "0.0.1"
+val cacheDir = layout.buildDirectory.dir("iCache").get()
 
 repositories {
     mavenCentral()
@@ -24,6 +28,17 @@ java {
 
 application {
     mainClass = "$group.Main"
+}
+
+tasks.register<Download>("downloadMcVersionManifest") {
+    src("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+    dest(cacheDir.file("all_versions_manifest.json"))
+    overwrite(false)
+    description = "Download the manifest JSON for MC jars."
+}
+
+tasks.run {
+    dependsOn("downloadMcVersionManifest")
 }
 
 tasks.test {
